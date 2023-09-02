efcbaf4434c668911c57b388bd237ebd51724ca4
JustSimon01 <simon.ch@yandex.ru>
2023-09-02 20:10:53 +0300

    feat(database): mongoDB + mongoose
    Добавил подключение к Монго и mongoose

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import mongoose from 'mongoose';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
