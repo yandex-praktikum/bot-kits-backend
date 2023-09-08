6f28aca9b77bef46fe851540a5ad3aa94a6e0463
pomazkovanton <vitrumbeta@gmail.com>
2023-09-08 22:58:49 +0800

    docs(platform): добавить swagger для модуля платформ #27

--- src/platform/dto/update-platform.dto.ts
+++ src/platform/dto/update-platform.dto.ts
@@ -7,36 +7,47 @@ import {
   ValidateNested,
 } from 'class-validator';
 import { Type } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
 
 class FormFields {
+  @ApiProperty({ example: true })
   @IsBoolean()
   @IsNotEmpty()
   name: boolean;
 
+  @ApiProperty({ example: true })
   @IsBoolean()
   @IsNotEmpty()
   pages: boolean;
 
+  @ApiProperty({ example: true })
   @IsBoolean()
   @IsNotEmpty()
   accessKey: boolean;
 
+  @ApiProperty({ example: false })
   @IsBoolean()
   @IsNotEmpty()
   url: boolean;
 }
 
 export class UpdatePlatformDto {
+  @ApiProperty({
+    example:
+      'https://cdn.icon-icons.com/icons2/1233/PNG/512/1492718766-vk_83600.png',
+  })
   @IsUrl()
   @IsOptional()
-  icon: string;
+  icon?: string;
 
+  @ApiProperty({ example: 'VK' })
   @IsString()
   @IsOptional()
-  title: string;
+  title?: string;
 
+  @ApiProperty()
   @ValidateNested()
   @Type(() => FormFields)
   @IsOptional()
-  formFields: FormFields;
+  formFields?: FormFields;
 }
--- src/platform/platform.controller.ts
+++ src/platform/platform.controller.ts
@@ -8,25 +8,58 @@ import {
   Post,
   Delete,
 } from '@nestjs/common';
+import {
+  ApiBody,
+  ApiCreatedResponse,
+  ApiForbiddenResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiParam,
+  ApiTags,
+  ApiUnprocessableEntityResponse,
+} from '@nestjs/swagger';
+
 import { PlatformService } from './platform.service';
 import { Platform } from './schema/platform.schema';
 import { CreatePlatformDto } from './dto/create-platform.dto';
 import { UpdatePlatformDto } from './dto/update-platform.dto';
 
+@ApiTags('Platforms')
 @Controller('platforms')
 export class PlatformController {
   constructor(private readonly platformService: PlatformService) {}
 
+  @ApiBody({ type: CreatePlatformDto })
+  @ApiCreatedResponse({
+    description: 'The record has been successfully created.',
+    type: Platform,
+  })
   @Post()
   create(@Body() createPlatformDto: CreatePlatformDto): Promise<Platform> {
     return this.platformService.create(createPlatformDto);
   }
 
+  @ApiOkResponse({
+    description: 'The resources were returned successfully',
+    type: [Platform],
+  })
+  @ApiForbiddenResponse({ description: 'Unauthorized Request' })
   @Get()
   findAll(): Promise<Platform[]> {
     return this.platformService.findAll();
   }
 
+  @ApiOkResponse({
+    description: 'The resource was returned successfully',
+    type: Platform,
+  })
+  @ApiForbiddenResponse({ description: 'Unauthorized Request' })
+  @ApiNotFoundResponse({ description: 'Resource not found' })
+  @ApiParam({
+    name: 'id',
+    description: 'Индификатор платформы',
+    example: '64f81ba37571bfaac18a857f',
+  })
   @Get(':id')
   async findOne(@Param('id') id: string): Promise<Platform> {
     const platform = await this.platformService.findOne(id);
@@ -34,6 +67,19 @@ export class PlatformController {
     return platform;
   }
 
+  @ApiOkResponse({
+    description: 'The resource was updated successfully',
+    type: Platform,
+  })
+  @ApiNotFoundResponse({ description: 'Resource not found' })
+  @ApiForbiddenResponse({ description: 'Unauthorized Request' })
+  @ApiUnprocessableEntityResponse({ description: 'Bad Request' })
+  @ApiBody({ type: UpdatePlatformDto })
+  @ApiParam({
+    name: 'id',
+    description: 'Индификатор платформы',
+    example: '64f81ba37571bfaac18a857f',
+  })
   @Patch(':id')
   update(
     @Param('id') id: string,
@@ -42,6 +88,17 @@ export class PlatformController {
     return this.platformService.update(id, updatePlatformDto);
   }
 
+  @ApiOkResponse({
+    description: 'The resource was returned successfully',
+    type: Platform,
+  })
+  @ApiForbiddenResponse({ description: 'Unauthorized Request' })
+  @ApiNotFoundResponse({ description: 'Resource not found' })
+  @ApiParam({
+    name: 'id',
+    description: 'Индификатор платформы',
+    example: '64f81ba37571bfaac18a857f',
+  })
   @Delete(':id')
   remove(@Param('id') id: string): Promise<Platform> {
     return this.platformService.remove(id);
