933e95627060a059e78e27af79071c64c8817ebe
pomazkovanton <vitrumbeta@gmail.com>
2023-09-16 21:34:53 +0800

    refactor(bot-temlate): прогнать код через линтеры #57

--- src/botTemplates/dto/update.bot-template.dto.ts
+++ src/botTemplates/dto/update.bot-template.dto.ts
@@ -1,29 +1,29 @@
-import { IsObject, IsOptional, IsString, IsUrl } from "class-validator";
-import { BotTemplate } from "../schema/bot-template.schema";
-import { ApiProperty } from "@nestjs/swagger";
+import { IsObject, IsOptional, IsString, IsUrl } from 'class-validator';
+import { BotTemplate } from '../schema/bot-template.schema';
+import { ApiProperty } from '@nestjs/swagger';
 
 export default class UpdateBotTemplateDto implements Partial<BotTemplate> {
-    @IsUrl()
-    @IsOptional()
-    @ApiProperty()
-    icon?: string;
+  @IsUrl()
+  @IsOptional()
+  @ApiProperty()
+  icon?: string;
 
-    @IsString()
-    @IsOptional()
-    @ApiProperty()
-    title?: string;
+  @IsString()
+  @IsOptional()
+  @ApiProperty()
+  title?: string;
 
-    @IsString()
-    @IsOptional()
-    @ApiProperty()
-    description?: string;
+  @IsString()
+  @IsOptional()
+  @ApiProperty()
+  description?: string;
 
-    @IsOptional()
-    @ApiProperty()
-    features?: string[]
+  @IsOptional()
+  @ApiProperty()
+  features?: string[];
 
-    @IsObject()
-    @IsOptional()
-    @ApiProperty()
-    settings?: Object
+  @IsObject()
+  @IsOptional()
+  @ApiProperty()
+  settings?: object;
 }
