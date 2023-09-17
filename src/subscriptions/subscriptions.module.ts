15b4934576a96dfa53ddf6c4e685db8a05674f27
Natalya Belikova <minaevanv@inbox.ru>
2023-09-17 12:13:08 +0300

    fix: добавить валидацию, дополнить связку с тарифом, обновить теги, доработать методы
    #48

--- src/subscriptions/subscriptions.module.ts
+++ src/subscriptions/subscriptions.module.ts
@@ -5,6 +5,7 @@ import { SubscriptionsController } from './subscriptions.controller';
 import { Subscription, SubscriptionSchema } from './schema/subscription.schema';
 import { Profile, ProfileSchema } from '../profiles/schema/profile.schema';
 import { Payment, PaymentSchema } from '../payments/schema/payment.schema';
+import { Tariff, TariffSchema } from 'src/tariffs/schema/tariff.schema';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { Payment, PaymentSchema } from '../payments/schema/payment.schema';
       { name: Subscription.name, schema: SubscriptionSchema },
       { name: Profile.name, schema: ProfileSchema },
       { name: Payment.name, schema: PaymentSchema },
+      { name: Tariff.name, schema: TariffSchema },
     ]),
   ],
   controllers: [SubscriptionsController],
