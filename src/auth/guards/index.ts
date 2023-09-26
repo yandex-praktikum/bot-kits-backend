68785a3225e9374bfc6fa9614e2341c8058f3977
pomazkovanton <vitrumbeta@gmail.com>
2023-09-26 14:03:08 +0800

    feat(vk-auth): добавить авторизация через вконтакте

--- src/auth/guards/index.ts
+++ src/auth/guards/index.ts
@@ -1,6 +1,13 @@
 import { GoogleGuard } from './google.guard';
 import { JwtGuard } from './jwtAuth.guards';
 import { LocalGuard } from './localAuth.guard';
+import { VkontakteGuard } from './vkontakte.guards';
 import { YandexGuard } from './yandex.guards';
 
-export const GUARDS = [LocalGuard, JwtGuard, YandexGuard, GoogleGuard];
+export const GUARDS = [
+  LocalGuard,
+  JwtGuard,
+  YandexGuard,
+  GoogleGuard,
+  VkontakteGuard,
+];
--- src/auth/strategies/index.ts
+++ src/auth/strategies/index.ts
@@ -1,6 +1,7 @@
 import { GoogleStrategy } from './google.strategy';
 import { JwtStrategy } from './jwt.strategy';
 import { LocalStrategy } from './local.strategy';
+import { VkontakteStrategy } from './vkontakte.strategy';
 import { YandexStrategy } from './yandex.strategy';
 
 export const STRTAGIES = [
@@ -8,4 +9,5 @@ export const STRTAGIES = [
   JwtStrategy,
   YandexStrategy,
   GoogleStrategy,
+  VkontakteStrategy,
 ];
