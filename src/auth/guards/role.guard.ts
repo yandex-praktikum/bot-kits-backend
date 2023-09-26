ce3f35ede30333ca9caf0bdfad60f44ed072f2ca
pomazkovanton <vitrumbeta@gmail.com>
2023-09-26 17:25:40 +0800

    feat(role-auth): добавить зашиту маршрутов по ролям

---
+++ src/auth/guards/role.guard.ts
@@ -0,0 +1,25 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { ProfilesService } from 'src/profiles/profiles.service';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(
+    private reflector: Reflector,
+    private readonly profileService: ProfilesService,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const roles = this.reflector.get<string[]>('roles', context.getHandler());
+    const request = context.switchToHttp().getRequest();
+
+    if (request?.user) {
+      const { id } = request.user;
+      const user = await this.profileService.findById(id);
+      const userRole = user.accounts[0].role;
+      return roles.includes(userRole);
+    }
+
+    return false;
+  }
+}
