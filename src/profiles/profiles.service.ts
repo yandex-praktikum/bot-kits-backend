ded9e9327accbff5096d3bcb46a139eb53286a0a
Catherine <cath.osipova@gmail.com>
2023-10-05 01:06:20 +0300

    feat(ratelimit): добавлен функционал
    Signed-off-by: Catherine <cath.osipova@gmail.com>

--- src/profiles/profiles.service.ts
+++ src/profiles/profiles.service.ts
@@ -1,17 +1,19 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { CreateProfileDto } from './dto/create-profile.dto';
-import { Profile } from './schema/profile.schema';
-import { Account } from 'src/accounts/schema/account.schema';
-import { UpdateProfileDto } from './dto/update-profile.dto';
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model, Types } from "mongoose";
+import { CreateProfileDto } from "./dto/create-profile.dto";
+import { Profile } from "./schema/profile.schema";
+import { Account } from "src/accounts/schema/account.schema";
+import { UpdateProfileDto } from "./dto/update-profile.dto";
 
 @Injectable()
 export class ProfilesService {
   constructor(
     @InjectModel(Profile.name) private profile: Model<Profile>,
-    @InjectModel(Account.name) private account: Model<Account>,
-  ) {}
+    @InjectModel(Account.name) private account: Model<Account>
+  ) {
+  }
+
   //profiles.service.ts
   async create(createProfileDto: CreateProfileDto) {
     const profileNew = new this.profile(createProfileDto);
