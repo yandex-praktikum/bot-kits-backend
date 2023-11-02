79f8943b2020814610fadbd89b88d3bcf80c4b77
theverybusybee <v_antonchik@mail.ru>
2023-11-02 18:00:38 +0300

    feat(statistics): schema for statistics module added

--- src/statistics/statistics.module.ts
+++ src/statistics/statistics.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { StatisticsController } from './statistics.controller';
+import { StatisticsService } from './statistics.service';
+import { Statistics, StatisticsSchema } from './schema/statistics.schema';
 
 @Module({
   imports: [
+    MongooseModule.forFeature([
+      { name: Statistics.name, schema: StatisticsSchema },
+    ]),
   ],
+  controllers: [StatisticsController],
+  providers: [StatisticsService],
 })
 export class StatisticsModule {}
