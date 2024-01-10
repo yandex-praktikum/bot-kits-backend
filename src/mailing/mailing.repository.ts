597478fc46788da9f4976a8c5cf9c187dc85bb85
knoxxrus <knoxxrus2@gmail.com>
2024-01-10 14:10:43 +0500

    feat(mailing): add mailing repository

--- src/mailing/mailing.repository.ts
+++ src/mailing/mailing.repository.ts
@@ -4,13 +4,70 @@ import {
   HttpStatus,
   Injectable,
   NotFoundException,
+  BadRequestException,
 } from '@nestjs/common';
 import { MongoServerError } from 'mongodb';
 import { InjectModel } from '@nestjs/mongoose';
-import { Mailing } from './schema/mailing.schema';
-import { Model } from 'mongoose';
+import { Mailing, MailingDocument } from './schema/mailing.schema';
+import { Model, Error } from 'mongoose';
+import { UpdateMailingDTO } from './dto/update-mailing.dto';
+import { CreateMailingDTO } from './dto/create-mailing.dto';
 
 @Injectable()
 export class MailingRepository {
-  constructor(@InjectModel(Mailing.name) private mailing: Model<Mailing>) {}
+  constructor(
+    @InjectModel(Mailing.name) private mailingModel: Model<MailingDocument>,
+  ) {}
+
+  async findById(id: string | number): Promise<Mailing> {
+    const post = await this.mailingModel.findById(id);
+
+    if (!post) {
+      throw new NotFoundException(`Post with ID ${id} not found`);
+    }
+
+    return await post.populate('platform');
+  }
+
+  async findAll(): Promise<Mailing[]> {
+    return await this.mailingModel.find().exec();
+  }
+
+  async remove(id: string | number): Promise<Mailing> {
+    return await this.mailingModel.findByIdAndRemove(id).exec();
+  }
+
+  async update(
+    updateMailingDTO: UpdateMailingDTO,
+    id: string | number,
+  ): Promise<Mailing> {
+    try {
+      const post = await this.mailingModel.findByIdAndUpdate(
+        id,
+        updateMailingDTO,
+        { new: true },
+      );
+
+      if (!post) throw new NotFoundException(`Post with ID ${id} not found`);
+
+      return await post.populate('platform');
+    } catch (err) {
+      if (err instanceof Error.CastError)
+        throw new BadRequestException('Invalid resource id');
+      throw err;
+    }
+  }
+
+  async create(createMailingDTO: CreateMailingDTO): Promise<Mailing> {
+    try {
+      const post = await this.mailingModel.create(createMailingDTO);
+      return await post.save();
+    } catch (err) {
+      if (err instanceof MongoServerError && err.code === 11000) {
+        throw new ConflictException('Такой пост уже существует');
+      }
+      //500й код
+      throw new Error('Что-то пошло не так');
+    }
+  }
 }
