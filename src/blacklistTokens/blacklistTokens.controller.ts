0454d7ef33ff123d6451f54b5280bb0d6b2d59aa
Stephen <stephen.jr@selyuk.com>
2024-02-04 23:00:55 +0200

    refactor: move lastactivity logic to blacklistTokens.repository.ts

--- src/blacklistTokens/blacklistTokens.controller.ts
+++ src/blacklistTokens/blacklistTokens.controller.ts
@@ -1,6 +1,5 @@
-import { Controller, Get, Headers, UseGuards } from '@nestjs/common';
+import { Controller, Get, Headers } from '@nestjs/common';
 import { BlacklistTokensService } from './blacklistTokens.service';
-import { JwtGuard } from 'src/auth/guards/jwtAuth.guards';
 import {
   ApiBearerAuth,
   ApiHeader,
@@ -8,18 +7,11 @@ import {
   ApiOperation,
   ApiTags,
 } from '@nestjs/swagger';
-import { InjectModel } from '@nestjs/mongoose';
-import { Profile, ProfileDocument } from '../profiles/schema/profile.schema';
-import { Model } from 'mongoose';
-import { JwtService } from '@nestjs/jwt';
-
 @ApiTags('logout')
 @Controller('logout')
 export class BlacklistTokensController {
   constructor(
     private readonly blacklistTokensService: BlacklistTokensService,
-    private readonly jwtService: JwtService,
-    @InjectModel(Profile.name) private profileModel: Model<ProfileDocument>,
   ) {}
 
   @Get()
@@ -46,17 +38,7 @@ export class BlacklistTokensController {
   })
   async addToken(@Headers('authorization') authHeader: string) {
     const token = authHeader.split(' ')[1];
-    // console.log(token)
     await this.blacklistTokensService.addToken(token);
-    const decoded = this.jwtService.decode(token);
-
-    if (decoded && decoded.sub) {
-      await this.profileModel.updateOne(
-        { _id: decoded.sub },
-        { $set: { lastAccountActivity: new Date() } },
-      );
-    }
-
-    return { message: 'User logged out' };
+    await this.blacklistTokensService.updateLastActivity(token);
   }
 }
